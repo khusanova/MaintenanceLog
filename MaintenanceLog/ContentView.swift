9b9de08816d0c0bc76b2b067ae3e034c9e427567
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-13 18:51:26 +0100

    Display log entries

--- MaintenanceLog/ContentView.swift
+++ MaintenanceLog/ContentView.swift
@@ -9,53 +9,23 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [LogEntry]
+    @Query private var entries: [LogEntry]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+        Text("Spaceship Home Maintenance Log: ")
+        ForEach(entries){
+            entry in Text(entry.content)
         }
     }
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: LogEntry.self, inMemory: true)
+    let container = try! ModelContainer(for: LogEntry.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(LogEntry.waterPlants)
+    container.mainContext.insert(LogEntry.vacuum)
+    container.mainContext.insert(LogEntry.cleanDishWasherFilter)
+    
+    return ContentView()
+        .modelContainer(container)
 }
