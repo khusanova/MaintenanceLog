04876309ae976b27e768efd013e2026d5fb6bc07
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-21 13:08:54 +0100

    Add alert_state and start_date to User

--- python-prototype/user.py
+++ python-prototype/user.py
@@ -1,6 +1,11 @@
+from datetime import date
+
+
 class User:
     def __init__(self, name: str, starship_name = None, log_entries = []):
         self.name = name
         self.starship_name = starship_name
         self.log_entries = log_entries
-        
+        self.alert_state = max(log_entry.alert_state for log_entry in
+                                   log_entries)
+        self.start_date = date.today()
