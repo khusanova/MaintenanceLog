0ae54296e80b54c80354ebe06483c3e7ee8feae1
Yanu <59870900+khusanova@users.noreply.github.com>
2026-02-21 14:52:07 +0100

    Add alert_durations function to User

--- python-prototype/user.py
+++ python-prototype/user.py
@@ -38,3 +38,22 @@ class User:
     @property
     def alert_color(self):
         return ALERTS[self.alert_state]
+
+    @property
+    def alert_durations_since_last_update(self):
+        today = date.today()
+        alert_start_dates = [today for _ in range(len(ALERTS))]
+        alert_start_dates[0] = self.date_last_update
+
+        for log_entry in self.log_entries:
+            last_state = log_entry.alert_state
+            if log_entry.next < alert_start_dates[last_state+1]:
+                alert_start_dates[last_state + 1] = log_entry.next
+
+        alert_durations = {ALERTS[i]: alert_start_dates[i+1].toordinal() -
+                              alert_start_dates[i].toordinal() for i in
+                           range(len(ALERTS)-1)}
+        alert_durations[ALERTS[-1]] = today.toordinal() - alert_start_dates[
+            -1].toordinal()
+
+        return alert_durations
